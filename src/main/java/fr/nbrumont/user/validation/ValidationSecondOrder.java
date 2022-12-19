fd2c1682fa12bc4187983d9b7f01caf1eb01f50e
nbrumont <nicolas.brumont-ext@pole-emploi.fr>
2022-12-19 13:56:14 +0100

    docs(): javadoc on validation order interface

--- src/main/java/fr/nbrumont/user/validation/ValidationSecondOrder.java
+++ src/main/java/fr/nbrumont/user/validation/ValidationSecondOrder.java
@@ -1,5 +1,10 @@
 package fr.nbrumont.user.validation;
 
+/**
+ * Interface created as a way to control the order of validation on different beans
+ * so that we can first send error messages regarding standard technical validation such as "not null" or "not empty"
+ * and then send fonctional error messages
+ */
 public interface ValidationSecondOrder {
 }
 
